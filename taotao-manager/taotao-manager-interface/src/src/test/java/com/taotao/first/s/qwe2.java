fa6c0c0593ba41eb83353dbe4fd66539b60f4aef
卢政诏 <690992117@qq.com>
2018-08-05 15:59:39 +0800

    2

---
+++ taotao-manager/taotao-manager-interface/src/src/test/java/com/taotao/first/s/qwe2.java
@@ -0,0 +1,5 @@
+package com.taotao.first.s;
+
+public class qwe2 {
+
+}
---
+++ taotao-manager/taotao-manager-interface/src/src/test/java/com/taotao/first/test.java
@@ -0,0 +1,10 @@
+package com.taotao.first;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
