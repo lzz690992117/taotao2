1575f22d91ca467a4943cafc3cf7694b83b13ea8
卢政诏 <690992117@qq.com>
2018-08-07 20:16:42 +0800

    123

--- taotao-portal-web/src/main/java/com/taotao/controller/portal/PortalController.java
+++ taotao-portal-web/src/main/java/com/taotao/controller/portal/PortalController.java
@@ -1,13 +1,54 @@
 package com.taotao.controller.portal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.Ad1Node;
+import com.taotao.common.util.JsonUtils;
+import com.taotao.content.service.ContentService;
+import com.taotao.pojo.TbContent;
 
 @Controller
 public class PortalController {
+	@Autowired
+	private ContentService contentService;
+	@Value("${AD1_CATEGORY_ID}")
+	private Long AD1_CATEGORY_ID;
+
+	@Value("${AD1_HEIGHT}")
+	private String AD1_HEIGHT;
+	@Value("${AD1_HEIGHT_B}")
+	private String AD1_HEIGHT_B;
+	@Value("${AD1_WIDTH}")
+	private String AD1_WIDTH;
+	@Value("${AD1_WIDTH_B}")
+	private String AD1_WIDTH_B;
+
 
 	@RequestMapping("/index")
-	public String index(){
+	public String list(Model model) {
+		List<TbContent> list = contentService.getContentListByCatId(AD1_CATEGORY_ID);
+		List<Ad1Node> nodes = new ArrayList<>();
+		for (TbContent tbContent : list) {
+			Ad1Node node = new Ad1Node();
+			node.setAlt(tbContent.getSubTitle());
+			node.setHref(tbContent.getUrl());
+			node.setSrc(tbContent.getPic());
+			node.setSrcB(tbContent.getPic2());
+			node.setHeight(AD1_HEIGHT);
+			node.setHeightB(AD1_HEIGHT_B);
+			node.setWidth(AD1_WIDTH);
+			node.setWidthB(AD1_WIDTH_B);
+			nodes.add(node);
+		}
+		model.addAttribute("ad1", JsonUtils.objectToJson(nodes));
 		return "index";
 	}
 }
